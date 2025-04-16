e2fbb282387dc0aebd6795bc6fb254106930d780
zuulaxx <93787256+zuulaxx@users.noreply.github.com>
2025-04-16 20:26:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const socket = io("https://alyclick.glitch.me");
 
 const scoreDisplay = document.getElementById("score");
 const clickBtn = document.getElementById("clickBtn");
+const playerCountDisplay = document.getElementById("playerCount");
 
-// Quand le bouton est cliqué, on envoie un "click" au serveur
 clickBtn.addEventListener("click", () => {
   socket.emit("click");
 });
 
-// Quand le serveur envoie une mise à jour du score
 socket.on("scoreUpdate", (score) => {
   scoreDisplay.textContent = score;
+});
+
+socket.on("playerCount", (count) => {
+  playerCountDisplay.textContent = count;
+
+  const container = document.getElementById("players");
+  container.classList.add("animated");
+
+  setTimeout(() => {
+    container.classList.remove("animated");
+  }, 300);
 });
